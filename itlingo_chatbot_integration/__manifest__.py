66ed8668598104408f224621c65b1dd6ab1c7b70
eduardo2715 <146322124+eduardo2715@users.noreply.github.com>
2026-05-26 17:27:49 +0100

    add new module for chatbot service integration

---
+++ itlingo_chatbot_integration/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "ITLingo Chatbot Integration",
+    "version": "1.0.0",
+    "summary": "Embed the external Django chatbot UI into workspace pages",
+    "category": "Website/Portal",
+    "author": "ITLingo",
+    "depends": [
+        "website",
+        "portal",
+        "itlingo_website_portal"
+    ],
+    "data": [
+        "views/templates.xml",
+        "data/ir_config_parameter.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
